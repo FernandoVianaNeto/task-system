68d1d167d568ee3da3a3f48fe3d449843c3423af
Fernando Viana <fernando.viana.nt@gmail.com>
2025-03-15 18:41:55 -0300

    feat: add jwt middleware to validate routes

---
+++ internal/infrastructure/web/middleware/jwt.middleware.go
@@ -0,0 +1,51 @@
+package middleware
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+	configs "task-system/cmd/config"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type JwtClaims struct {
+	Email string `json:"email"`
+	Role  string `json:"role"`
+	jwt.RegisteredClaims
+}
+
+func JwtAuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		secretKey := []byte(configs.ApplicationCfg.PasswordSecretHash)
+
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Token not provided"})
+			return
+		}
+
+		tokenString := strings.Replace(authHeader, "Bearer ", "", 1)
+
+		token, err := jwt.ParseWithClaims(tokenString, &JwtClaims{}, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("unexpected assign method: %v", token.Header["alg"])
+			}
+			return secretKey, nil
+		})
+
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Invalid token"})
+			return
+		}
+
+		if claims, ok := token.Claims.(*JwtClaims); ok && token.Valid {
+			c.Set("user_email", claims.Email)
+			c.Set("user_role", claims.Email)
+			c.Next()
+		} else {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Invalid token"})
+		}
+	}
+}
--- internal/infrastructure/web/routes.go
+++ internal/infrastructure/web/routes.go
@@ -1,19 +1,21 @@
 package web
 
 import (
+	"task-system/internal/infrastructure/web/middleware"
+
 	"github.com/gin-gonic/gin"
 )
 
 func Routes(engine *gin.Engine, server *Server) *gin.Engine {
 	{
-		task := engine.Group("/task")
+		task := engine.Group("/task", middleware.JwtAuthMiddleware())
 		{
 			task.POST("/", server.CreateTaskHandler)
 		}
 	}
 
 	{
-		user := engine.Group("/user")
+		user := engine.Group("/user", middleware.JwtAuthMiddleware())
 		{
 			user.POST("/", server.CreateUserHandler)
 			user.GET("/:uuid", server.GetUserHandler)
